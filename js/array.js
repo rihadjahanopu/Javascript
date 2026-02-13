cd0b0cd6e7a0dc87a2196987b8220a69c5822417
Rihad Jahan Opu <rihadjahanopu@gmail.com>
2026-02-13 21:14:30 +0600

    🚧 WIP: ok

--- js/array.js
+++ js/array.js
@@ -13,7 +13,7 @@ console.log("1️⃣ ARRAY CREATION METHODS");
 console.log("---------------------------");
 
 // Literal Notation (Most Common)
-const fruits = ["Apple", "Banana", "Mango", "lemon", "Banana", "lili"];
+const fruits = ["Apple", "Banana", "Mango", "lemon", "Banane", "lili"];
 console.log("Literal:", fruits); // ['Apple', 'Banana', 'Mango']
 
 // Array Constructor
